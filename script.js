4e6688e7062d1a268f7c40edf7690090f533f039
navjotmaan <navjotmaan137@gmail.com>
2025-10-06 15:45:21 +0000

    added style to the webpage and fix the ui

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const container = document.querySelector('.weather-data');
 const icons = {
     'rain': 'cloud-with-rain.png',
     'clear-day': 'sun.png',
-    'cloudy-day': 'partly-cloudy-day.png',
+    'cloudy': 'partly-cloudy-day.png',
     'partly-cloudy-day': 'partly-cloudy-day.png'
 }
 
@@ -57,17 +57,24 @@ function displayWeather(day) {
     const div = document.createElement('div');
     div.classList.add('info-box');
 
-    const iconFile = icons[day.icon] || './resources/partly-cloudy-day.png';
+    const iconFile = icons[day.icon];
 
     const img = document.createElement('img');
+    img.classList.add('icon');
     img.src = `./resources/${iconFile}`;
+    img.alt = 'weather icon';
 
-    div.innerHTML = `<p>Date: ${day.date}</p>
+    const des = document.createElement('p');
+    des.setAttribute('id', 'des');
+    des.textContent = day.description;
+
+    div.innerHTML = `
+                    <p>${day.date}</p>
                     <p>Temperature: ${day.temp}
-                    <p>Description: ${day.description}</p>
                     <p>Longitude: ${day.longitude}</p>
                     <p>Latitide: ${day.latitude}</p>`;
 
-    div.prepend(img);              
+    div.prepend(img); 
+    div.prepend(des);             
     container.appendChild(div);
 }
