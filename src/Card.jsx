e8e91b4b487e1fc8a526a64e11f982d3db1c912c
navjotmaan <navjotmaan137@gmail.com>
2026-02-19 04:56:52 +0000

    add weather icon in weather card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,23 @@
+import rainIcon from './assets/cloud-with-rain.png';
+import sunIcon from './assets/sun.png';
+import cloudIcon from './assets/partly-cloudy-day.png';
+import snowIcon from './assets/snow.png';
+
+const icons = {
+    'rain': rainIcon,
+    'clear-day': sunIcon,
+    'cloudy': cloudIcon,
+    'snow': snowIcon,
+    'partly-cloudy-day': cloudIcon
+};
+    
 const WeatherCard = ({ day }) => {
+
+    const iconImg = icons[day.icon];
     return (
         <div className="weather-card">
             <p><b>{day.description}</b></p>
-            <p>{day.icon}</p>
+            <img src={iconImg} alt={day.icon} width='48px'/>
             <p><i>Date: </i>{day.date}</p>
             <p><i>Temperature: </i>{day.temp}</p>
             <p><i>Longitude: </i>{day.longitude}</p>
