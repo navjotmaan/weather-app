f7df4d2e2ea7971b65789a49ecb431326af88217
navjotmaan <navjotmaan137@gmail.com>
2026-02-19 15:53:35 +0000

    refactor the fetching from promise to async/await and display the hours data for today

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -1,37 +1,57 @@
 import { useEffect, useState } from 'react'
-import WeatherCard from './Card';
+import { WeatherCard, HourlyForecast } from './Card';
 
 const Fetch = ({ place }) => {
+    const [location, setLocation] = useState('');
     const [selectedData, setSelectedData] = useState([]);
 
     useEffect(() => {
-        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=JA6Z2MJZXFLT6SJCCHDRUASRM`)
-        .then((res) =>  res.json())
-        .then((data) => {
-        const weather = data.days.slice(0, 6)
-                .map(day => ({
-                    location: data.address,
-                    icon: day.icon,
-                    id: day.datetimeEpoch,
-                    date: day.datetime,
-                    temp: day.temp,
-                    description: day.description,
-                    longitude: data.longitude, 
-                    latitude: data.latitude
-            }));
-        setSelectedData(weather);
-        })
-        .catch((err) => console.error("Error fetching weather data", err));
+        const getData = async () => {
+            try {
+                const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=JA6Z2MJZXFLT6SJCCHDRUASRM`);
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+
+                setLocation(data.address);
+                
+                const weather = data.days.slice(0, 5)
+                    .map(day => ({
+                        id: day.datetimeEpoch,
+                        date: day.datetime,
+                        temp: day.temp,
+                        icon: day.icon,
+                        hours: day.hours.map(hour => ({
+                            id: hour.datetimeEpoch,
+                            icon: hour.icon,
+                            time: hour.datetime,
+                            temp: hour.temp,
+                        })),
+                }));
+                setSelectedData(weather);
+            } catch (err) {
+                console.error("Error fetching weather data", err);
+            }
+        };
+        getData();
     }, [place]);
 
     return (
-        <div className='weather-area'>
+        <div>
             {selectedData.length === 0 ? (
                 <p>Loading...</p>
             ): <main>
-                    <h2>Location: {selectedData[0].location}</h2>
+                    <h2>{location}</h2>
+
+                    <h3>Today</h3>
+                    <HourlyForecast allHoursData={selectedData[0].hours} />
+
+                    <h3>Next Forecast</h3>
                     <div className='cards-area'>
-                        {selectedData.map((day) => (
+                        {selectedData.slice(1).map((day) => (
                             <WeatherCard key={day.id} day={day} />
                         ))}
                     </div>
@@ -39,6 +59,6 @@ const Fetch = ({ place }) => {
             }   
         </div>
     )
-}
+};
 
 export default Fetch;
